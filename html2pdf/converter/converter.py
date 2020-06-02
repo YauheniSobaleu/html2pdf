ffea72269eeec5ba7ae1a7803a66a63f5d1cc3d0
Eugene Sobolev <devteslaman@gmail.com>
2020-06-02 18:16:23 +0300

    Implement erorrs handling and write docstrings.

--- html2pdf/converter/converter.py
+++ html2pdf/converter/converter.py
@@ -6,20 +6,39 @@ from urllib.parse import urlparse
 import pdfkit
 
 
+class ConvertingError(Exception):
+    """
+    This exception represents an error during converting.
+    In example, when Host of a url is unreachable.
+
+    In other words, this is a wrapper for wkhtmltopdf errors.
+    """
+    pass
+
+
 def url_to_pdf(url: str) -> IO:
+    """Fetch HTML from url and convert the page to pdf,"""
     with NamedTemporaryFile('w+b') as tmpf:
-        pdfkit.from_url(url, tmpf.name)
+        try:
+            pdfkit.from_url(url, tmpf.name)
+        except OSError as e:
+            raise ConvertingError from e
         pdf = io.BytesIO(tmpf.read())
     return pdf
 
 
 def html_to_pdf(html: str) -> IO:
+    """Convert HTML string to pdf."""
     with NamedTemporaryFile('w+b') as tmpf:
-        pdfkit.from_string(html, tmpf.name)
+        try:
+            pdfkit.from_string(html, tmpf.name)
+        except OSError as e:
+            raise ConvertingError from e
         pdf = io.BytesIO(tmpf.read())
     return pdf
 
 
 def filename_from_url(url: str) -> str:
+    """Generate pdf filename using a hostname of a URL."""
     parsed = urlparse(url)
     return parsed.hostname + '.pdf'
